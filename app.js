8ab4233c858c9681fafe416bf3077be1742b97a3
Didier <didier31cr@hotmail.com>
2020-03-07 19:47:56 -0600

    Adding queue's file

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-console.log(`Hello World!`);
+  class Queue{
+
+    constructor(){
+        this.items = {};
+        this.start = 0;
+        this.end =0;
+    };
+    
+    enqueue(data){
+        this.items[this.end] = data;
+        this.end++;
+    };
+
+    dequeue(){
+        if (this.start === this.end) {
+            return console.log(`The queue is empty`);
+        } else {
+            const data = this.items[this.start];
+            delete this.items[this.start];
+            this.start++;
+            return data;
+        }
+    };
+
+    size(){
+        return this.end - this.start;
+    };
+
+    empty(){
+        if (this.size() === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    peek(){
+        if (this.size() === 0) {
+            return console.log(`The queue is empty`);
+        } else {
+            return this.items[this.start];
+        }
+    };
+
+    print(){
+        if (this.size() === 0) {
+            return console.log(`The queue is empty`);
+        } else {
+            let values = '';
+            for (let i = 0; i < this.end; i++) {
+                values += this.items[i] + " ";
+                
+            };
+
+            return values;
+        }
+    };
+}
+
+module.exports = Queue;
