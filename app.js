92d37a16652f33df582a1162cd6fb392671764b3
Didier <didier31cr@hotmail.com>
2020-03-07 19:59:32 -0600

    Adding stack's file

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-console.log(`Hello World!`);
+class Stack{
+
+    constructor(){
+        this.items = {};
+        this.top = 0;
+
+    };
+
+    push(data){
+        this.top++;
+        this.items[this.top] = data;
+    };
+
+    pop(){
+        let deletedItem;
+        if (this.top === 0) {
+            return console.log(`This stack is empty`);
+        } else {
+            deletedItem = this.items[this.top];
+            delete this.items[this.top];
+            this.top--;
+            return deletedItem;
+        };
+
+    };
+
+    size(){
+        return this.top;
+    };
+
+    empty(){
+        if (this.size() === 0) {
+            return true;
+        } else {
+            return false;
+        };
+    };
+
+    peek(){
+        if (this.size() === 0) {
+            return console.log(`This stack is empty`);
+        } else {
+            return this.items[this.top];
+        };
+
+    };
+
+    print(){
+        let values = '';
+        for (let i = this.top; i > 0; i--) {
+            values += this.items[i] + ' ';
+        };
+           return values;
+    };
+
+};
+
+module.exports = Stack;
