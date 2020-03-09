0d05607967d7c7e0cf55d01258ac3e89a83dcfb4
Didier Jesús Hernández Avendaño <didier.hernandez@avantica.net>
2020-03-09 15:08:40 -0600

    Adding module pattern design's file

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log(`Hello World!`);
+const modulePattern = (function(){
+
+    let text = "You can't access this text if you don't call this variable on the return area";
+
+    return {
+        showText: function(){
+            console.log(text);
+        }
+    }
+
+    
+})();
+
+console.log(modulePattern.showText());
+
+module.exports = modulePattern;
